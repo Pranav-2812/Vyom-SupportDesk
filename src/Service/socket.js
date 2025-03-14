3debf9bc715438e8bc1e51788a58d4cc35c1ce62
Pranav-2812 <pranavdhumale2812@gmail.com>
2025-03-14 23:23:01 +0530

    Changed socket server

--- src/Service/socket.js
+++ src/Service/socket.js
@@ -1,3 +1,3 @@
 import { io } from "socket.io-client";
-const socket= io("http://127.0.0.1:5000");
+const socket= io("https://vyom-supportdesk-signaling-server.onrender.com");
 export default  socket;
